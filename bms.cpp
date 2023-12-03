0328605d4903450ac72abf6e1f433b5d72b7a295
Abhinandan Singh <singhabhinandan614@gmail.com>
2023-12-03 13:47:58 +0530

    Created Account class

--- bms.cpp
+++ bms.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
 using namespace std;
 
+class Account {
+    string name;
+    string password;
+    
+    double balance;
+
+    public: 
+    double getBalance() {};
+    void deposit(double amount) {
+        cout << "Deposit of INR " << amount << " is successfull\n";
+    };
+    void withdraw(double amount) {
+        cout << "Withdrawl of INR " << amount << " is successfull\n";
+    };
+    void openAccount(double amount) {
+        cout << "Account Opening with deposit INR " << amount << " is successfull\n";
+    };
+    
+};
+
 int main() {
+    Account ac1;
+    
+    int choice;
+    double amount;
+    string user_id, user_pwd;
+    
     return 0;
 }
